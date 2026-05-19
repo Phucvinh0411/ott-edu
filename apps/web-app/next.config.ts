f44432f258cd7fdb55e25cbfcbf2739fa4d8529f
ToHoangThanh042004 <Thanhsgk123@gmail.com>
2026-05-19 23:14:37 +0700

    fix next.js workspace root

--- apps/web-app/next.config.ts
+++ apps/web-app/next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    root: process.cwd(),
+  },
+
   // THÊM PHẦN IMAGES VÀO ĐÂY:
   images: {
     remotePatterns: [
